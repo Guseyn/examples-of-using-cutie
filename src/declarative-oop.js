c3d4bf285988e128292540cda4ffee104999baf6
guseyn <guseynism@gmail.com>
2019-07-22 22:18:39 +0400

    add script for video

---
+++ src/declarative-oop.js
@@ -0,0 +1,7 @@
+
+const { ReadDataByPath, WrittenFile } = require('@cuties/fs')
+
+new WrittenFile(
+  './text2.txt',
+  new ReadDataByPath('./text1.txt', 'utf8')
+).call()
---
+++ src/promise.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+
+const read = (path) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, result) => {
+      if (err != null) {
+        reject(err)
+      } else {
+        resolve(result)
+      }
+    })
+  })
+}
+
+const write = (path, content) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(path, content, (err, result) => {
+      if (err != null) {
+        reject(err)
+      } else {
+        resolve(result)
+      }
+    })
+  })
+}
+
+read('./text1.txt').then((data) => {
+  return write('./text2.txt', data)
+}).catch((error) => {
+  // handling error
+})
